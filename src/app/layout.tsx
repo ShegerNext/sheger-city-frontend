8ff5c9b287baa396c2d7428496450bad7aff5868
anteneh83 <antenehgetnet83@gmail.com>
2025-09-08 18:36:07 -0900

    add language feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,31 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Header } from "@/components/Header";
 import "./globals.css";
+import { LanguageProvider } from "@/context/LanguageContext";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <Header />
-          <main className="p-6">{children}</main>
-        </body>
-      </html>
+      <LanguageProvider>
+        <html lang="en">
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            <Header />
+            <main className="p-6">{children}</main>
+          </body>
+        </html>
+      </LanguageProvider>
     </ClerkProvider>
   );
 }
